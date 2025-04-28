7dccaddbc076c20bd37f97d51853fccb7b5bb268
alexesp <alexizaes@hotmail.com>
2025-04-29 01:27:11 +0200

    Agregar archivos de proyecto.

---
+++ HybridTodoLibrary/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HybridTodoLibrary
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RazorLibrary/Models/TodoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace RazorLibrary.Models
+{
+    public class TodoModel
+    {
+        [SupplyParameterFromForm]
+        public string TodoItem { get; set; } = string.Empty;
+        public bool IsComplete { get; set; }
+    }
+}
